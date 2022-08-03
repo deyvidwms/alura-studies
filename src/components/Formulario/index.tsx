5845dedef8b31946f0752c06014169372e29b526
Deyvid William <deyvidwms@gmail.com>
2022-08-03 16:30:06 -0300

    Aplicando css modules no projeto

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Botao from '../Botao';
-import './style.scss';
+import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
   render () {
     return (
-      <form className='novaTarefa'>
+      <form className={style.novaTarefa}>
 
-        <div className='inputContainer'>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input 
             type="text" 
@@ -18,7 +18,7 @@ class Formulario extends React.Component {
           />
         </div>
 
-        <div className='inputContainer'>
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">Tempo</label>
           <input 
             type="time" 
