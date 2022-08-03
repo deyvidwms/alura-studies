1ccdc912469a230713dcda2c956b99d5b9e2eaad
Deyvid William <deyvidwms@gmail.com>
2022-08-03 18:05:31 -0300

    Funcionalidade de adicionar tarefa

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import style from './Botao.module.scss'
 
-class Botao extends React.Component<{children: string}> {
+class Botao extends React.Component<{
+  type?: "button" | "submit" | "reset" | undefined, 
+  children: string
+}> {
   render() {
+    const { type="button", children } = this.props;
     return (
-      <button className={style.botao}>
-        {this.props.children}
+      <button type={type} className={style.botao}>
+        {children}
       </button>
     )
   }
